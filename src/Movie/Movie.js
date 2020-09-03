d325181e84c3841177c0ca54289ce89c3dc14362
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-03 12:48:34 -0600

    remove titles from poster images

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -4,13 +4,13 @@ import './Movie.css';
 import heartFavoriteFalse from '../images/heart-outline.png';
 import heartFavoriteTrue from '../images/heart.png';
 
-const Movie = ({ id, title, averageRating, backdropPath, rating, favorites, loggedIn, home }) => {
+const Movie = ({ id, title, averageRating, posterPath, rating, favorites, loggedIn, home }) => {
   const inFavorites = favorites.find(movieId => movieId === id);
 
   return (
     <>
       {home &&
-        <section className='Movie' aria-label='movie-overview' style={{ backgroundImage: `url(${backdropPath})` }} id={id} alt={title}>
+        <section className='Movie' aria-label='movie-overview' style={{ backgroundImage: `url(${posterPath})` }} id={id} alt={title}>
           {inFavorites && loggedIn &&
             <img className='heart' src={heartFavoriteTrue} id={`heart${id}`} alt='favorited' onClick={(event) => {event.preventDefault()}}/>
           }
@@ -18,17 +18,15 @@ const Movie = ({ id, title, averageRating, backdropPath, rating, favorites, logg
             <img className='heart' src={heartFavoriteFalse} id={`heart${id}`} alt='not favorited' onClick={(event) => {event.preventDefault()}}/>
           }
           <p className='movie-rating'>{Math.round(averageRating * 10) / 10} / 10</p>
-          <h3 className='movie-title'>{title}</h3>
           {rating && 
             <p className='user-rating'>Your rating: {rating.rating} / 10</p>
           }
         </section>
       }
       {!home && 
-        <section className='Movie' aria-label='movie-overview' style={{ backgroundImage: `url(${backdropPath})` }} id={id} alt={title}>
+        <section className='Movie' aria-label='movie-overview' style={{ backgroundImage: `url(${posterPath})` }} id={id} alt={title}>
           <img className='heart' src={heartFavoriteTrue} id={`heart${id}`} alt='favorited' onClick={(event) => {event.preventDefault()}}/>
           <p className='movie-rating'>{Math.round(averageRating * 10) / 10} / 10</p>
-          <h3 className='movie-title'>{title}</h3>
           {rating &&
             <p className='user-rating'>Your rating: {rating.rating} / 10</p>
           }
@@ -42,7 +40,7 @@ Movie.propTypes = {
   id: PropTypes.number,
   title: PropTypes.string,
   averageRating: PropTypes.number,
-  backdropPath: PropTypes.string,
+  posterPath: PropTypes.string,
   rating: PropTypes.object,
   favorites: PropTypes.array,
   loggedIn: PropTypes.bool,
