141628a499b6226720202a2c7ed2ec285e603c91
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-03 12:11:27 -0600

    refactor fetch api

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,10 +1,10 @@
-export const getMovies = async () => {
+export async function getMovies() {
   const response = await fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies');
   const movies = await checkResponse(response); 
   return movies;
 }
 
-export const checkLoginCredentials = async (loginInfo) => {
+export async function checkLoginCredentials(loginInfo) {
   const response = await fetch('https://rancid-tomatillos.herokuapp.com/api/v2/login', {
     method: 'POST',
     headers: {
@@ -16,13 +16,13 @@ export const checkLoginCredentials = async (loginInfo) => {
   return validation;
 }
 
-export const fetchUserRatings = async (userId) => {
+export async function fetchUserRatings(userId) {
   const response = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/users/${userId}/ratings`);
   const userRatings = await checkResponse(response);
   return userRatings; 
 }
 
-export const postNewRating = async (userId, movieId, userRating) => {
+export async function postNewRating(userId, movieId, userRating) {
   const postResponse = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/users/${userId}/ratings`, {
     method: 'POST',
     headers: {
@@ -39,13 +39,13 @@ export const postNewRating = async (userId, movieId, userRating) => {
   return response; 
 }
 
-export const getFavoriteMovies = async () => {
+export async function getFavoriteMovies() {
   const response = await fetch('http://localhost:3001/api/v1/favorites');
   const favoriteMovies = await checkResponse(response); 
   return favoriteMovies; 
 }
 
-export const postFavoriteMovie = async (id) => {
+export async function postFavoriteMovie(id) {
   const response = await fetch('http://localhost:3001/api/v1/favorites', {
     method: 'POST',
     headers: {
@@ -61,7 +61,7 @@ export const postFavoriteMovie = async (id) => {
   return parsedResponse; 
 }
 
-export const deleteRating = async (userId, ratingId) => {
+export async function deleteRating(userId, ratingId) {
   const deleteResponse = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/users/${userId}/ratings/${ratingId}`, {
     method: 'DELETE',
     headers: {
@@ -75,13 +75,13 @@ export const deleteRating = async (userId, ratingId) => {
   }
 }
 
-export const getComments = async (movieId) => {
+export async function getComments (movieId) {
   const response = await fetch(`http://localhost:3001/api/v1/movies/${movieId}/comments`);
   const comments = await checkResponse(response);
   return comments;
 }
 
-export const postComment = async (movieId, author, comment) => {
+export async function postComment(movieId, author, comment) {
 const response = await fetch(`http://localhost:3001/api/v1/movies/${movieId}/comments`, {
     method: 'POST',
     headers: {
