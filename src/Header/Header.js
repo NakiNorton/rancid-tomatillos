99a40647af8d3e8f9f76658154876cee28858a4e
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-03 18:16:29 -0600

    Add image to favorites nav link

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import '../Header/Header.css';
 import { NavLink, Link } from 'react-router-dom';
 import yellowTomatoIcon from '../images/yellowTomato.png';
+import FavoritesIcon from '../images/tomato.png';
 
 const Header = ({ loggedIn, updateLoginStatus, updateUserId }) => {
   return (
@@ -13,14 +14,16 @@ const Header = ({ loggedIn, updateLoginStatus, updateUserId }) => {
         <ul>
           <li><NavLink to='/' className='nav-link active' aria-label='Go home'>HOME</NavLink></li>
           {loggedIn &&
-            <li><NavLink to='/favorites' className='nav-link active' aria-label="View favorites">FAVORITES</NavLink></li>
+            <li>
+            <img className='fav-icon' src={FavoritesIcon} />
+              <NavLink to='/favorites' className='nav-link active' aria-label="View favorites">FAVORITES</NavLink></li>
           }
         
         {loggedIn &&
-            <li><Link to='/'><button onClick={() => {
+            <li><Link to='/' className='nav-link active' onClick={() => {
             updateLoginStatus(false);
             updateUserId(null);
-          }}>LOG OUT</button></Link></li>
+          }}>LOG OUT</Link></li>
         }
         {!loggedIn &&
             <li><Link to='/login' className='nav-link active'>LOG IN</Link></li>
