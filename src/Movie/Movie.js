40cdda364f3d939675fc38955b0b404d75a11bdb
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-03 18:16:46 -0600

    Edit styling to movie cards

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
-import heartFavoriteFalse from '../images/tomatoOutline.png';
+import heartFavoriteFalse from '../images/yellowTomato.png';
 import heartFavoriteTrue from '../images/tomato.png';
 
 const Movie = ({ id, title, averageRating, posterPath, rating, favorites, loggedIn, home }) => {
@@ -10,26 +10,27 @@ const Movie = ({ id, title, averageRating, posterPath, rating, favorites, logged
   return (
     <>
       {home &&
+      <div className='movie-card'>
         <section className='Movie' aria-label='movie-overview' style={{ backgroundImage: `url(${posterPath})` }} id={id} alt={title}>
-          {inFavorites && loggedIn &&
-            <img className='heart' src={heartFavoriteTrue} id={`heart${id}`} alt='favorited' onClick={(event) => {event.preventDefault()}}/>
-          }
-          {!inFavorites && loggedIn &&
-            <img className='heart' src={heartFavoriteFalse} id={`heart${id}`} alt='not favorited' onClick={(event) => {event.preventDefault()}}/>
-          }
           <p className='movie-rating'>{Math.round(averageRating * 10) / 10} / 10</p>
-          {rating && 
-            <p className='user-rating'>Your rating: {rating.rating} / 10</p>
-          }
         </section>
+          {loggedIn &&
+          <section className='movie-card-info' onClick={(event) => { event.preventDefault() }}>
+            <img className='heart' src={inFavorites ? heartFavoriteTrue : heartFavoriteFalse} id={`heart${id}`} alt='favorited' onClick={(event) => { event.preventDefault() }} />
+          {rating &&
+        <p className='user-rating'>Your rating: {rating.rating} / 10</p>
+          } 
+        </section>
+        }
+      </div>
       }
       {!home && 
         <section className='Movie' aria-label='movie-overview' style={{ backgroundImage: `url(${posterPath})` }} id={id} alt={title}>
-          <img className='heart' src={heartFavoriteTrue} id={`heart${id}`} alt='favorited' onClick={(event) => {event.preventDefault()}}/>
-          <p className='movie-rating'>{Math.round(averageRating * 10) / 10} / 10</p>
+          <img className='heart' style={{ opacity: '0.9' }} src={heartFavoriteTrue} id={`heart${id}`} alt='favorited' onClick={(event) => {event.preventDefault()}}/>
+          {/* <p className='movie-rating'>{Math.round(averageRating * 10) / 10} / 10</p>
           {rating &&
             <p className='user-rating'>Your rating: {rating.rating} / 10</p>
-          }
+          } */}
         </section>
       }
     </>
